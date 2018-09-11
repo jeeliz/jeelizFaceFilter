68cbd098c9cd26281bf0f3ca37b0e44898259c8d
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-10 23:18:50 -0400

    update

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -33,17 +33,12 @@ document.head.appendChild(script);
  	 	audioURL = window.URL.createObjectURL(blob);
 		console.log("heres the file url",audioURL);
 
-	var link = document.createElement("a"); // Or maybe get it from the current document
-	link.href = audioURL;
-	link.download = "aDefaultFileName.mp4";
-	link.innerHTML = "FILE FROM CANVAS";
-    link.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
-    //link.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
-   // link.setAttribute('alt', 'Fork me on GitHub');
-   // link.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
+		var link = document.createElement("a"); // Or maybe get it from the current document
+		link.href = audioURL;
+		link.download = "aDefaultFileName.mp4";
+		link.innerHTML = "FILE FROM CANVAS";	
+		link.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
 		document.body.appendChild(link); 
-
-		alert(link);
 	} // end of media record stop
 	setTimeout(function(){mediaRecorder.stop()},10*1000);
 	// Create a second file stream too. for raw data
@@ -51,9 +46,10 @@ document.head.appendChild(script);
 	//var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(25);
 	navigator.mediaDevices.getUserMedia({"video":true,"audio":true},onMediaSuccess,onMediaError);
 	function onMediaError(e){
-		console.error('media error',e);
+		console.log('media error',e);
 	}
 	function onMediaSuccess(stream){
+		console.log("media success");
 		var mediaRecorder2 = new MediaRecorder(stream);
 		mediaRecorder2.start();
 		var chunks = [];
