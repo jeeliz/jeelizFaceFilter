b1d3cdfbb3b786f2250bb7e0d9ac5a4ebb41b8ae
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-10 23:17:07 -0400

    update

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -13,6 +13,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     document.body.appendChild(githubRibbon);
 */
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.webrtc-experiment.com/MediaStreamRecorder.js';
+document.head.appendChild(script);
+
 	console.log("dom content loaded");
 	var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(25);
 	var mediaRecorder = new MediaRecorder(stream);
@@ -31,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var link = document.createElement("a"); // Or maybe get it from the current document
 	link.href = audioURL;
 	link.download = "aDefaultFileName.mp4";
-	link.innerHTML = "Click here to download the file";
+	link.innerHTML = "FILE FROM CANVAS";
     link.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
     //link.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
    // link.setAttribute('alt', 'Fork me on GitHub');
@@ -43,6 +48,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	setTimeout(function(){mediaRecorder.stop()},10*1000);
 	// Create a second file stream too. for raw data
 
+	//var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(25);
+	navigator.mediaDevices.getUserMedia({"video":true,"audio":true},onMediaSuccess,onMediaError);
+	function onMediaError(e){
+		console.error('media error',e);
+	}
+	function onMediaSuccess(stream){
+		var mediaRecorder2 = new MediaRecorder(stream);
+		mediaRecorder2.start();
+		var chunks = [];
+		mediaRecorder2.ondataavailable=function(e){
+			chunks.push(e.data)
+		}
+		mediaRecorder2.onstop=function(e){
+			var blob = new Blob(chunks, { 'type' : 'video/mp4' });
+	 		 chunks = [];
+ 	 		var audioURL = window.URL.createObjectURL(blob);
+			console.log("heres the file url",audioURL);
+	
+			var link = document.createElement("a"); // Or maybe get it from the current document
+			link.href = audioURL;
+			link.download = "aDefaultFileName.mp4";
+			link.innerHTML = "FILE FROM WEBCAM";
+   			 link.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
+			document.body.appendChild(link); 
+		} // end of media record stop
+		setTimeout(function(){mediaRecorder2.stop()},10*1000);
+	// Create a second file stream too. for raw data
+	}
 
 	
 });
