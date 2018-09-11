368545d9ca6c71fd7df9253fd03d3328c3283a78
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-10 21:24:41 -0400

    update install

--- httpsServer.py
+++ httpsServer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
 import BaseHTTPServer, SimpleHTTPServer
+#import http.server;
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+#httpd = http.server.HTTPServer(('0.0.0.0', 443), http.server.SimpleHTTPRequestHandler)
+try:
+	httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./fullchain.pem',keyfile='./privkey.pem',server_side=True,ssl_version=ssl.PROTOCOL_TLSv1_2)
+except:
+	httpd.socket = ssl.wrap_socket (httpd.socket, certfile='../fullchain.pem',keyfile='../privkey.pem',server_side=True,ssl_version=ssl.PROTOCOL_TLSv1_2)
 httpd.serve_forever()
+
