cec8c5dd5215835ea3f089dbce1a66e206707d1f
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-10 21:39:20 -0400

    update

--- httpsServer.py
+++ httpsServer.py
@@ -4,7 +4,7 @@ import BaseHTTPServer, SimpleHTTPServer
 #import http.server;
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
 #httpd = http.server.HTTPServer(('0.0.0.0', 443), http.server.SimpleHTTPRequestHandler)
 try:
 	httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./fullchain.pem',keyfile='./privkey.pem',server_side=True,ssl_version=ssl.PROTOCOL_TLSv1_2)
