3afff599711842bfcceb4b6289c7e8680d825218
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-11 16:41:12 -0400

    add missing libs

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -98,194 +98,196 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	    document.body.innerHTML= this.responseText+" "+document.body.innerHTML;
 		main();
+		headerLoaded();
 	};
 	xhr.send();
-	//document.body.innerHTML='<style>html { height: 100%;}body { min-height: 100%;}.mycanvas {position:absolute;}.canvasContainer {position: relative;margin: 0 auto;text-align: center; }#myProgress{position:relative;z-index:99 !important;}#pleaseWait{position:relative;z-index:99 !important;text-align:center;color:red;}#recordingInstructionsStyle{position:relative;text-align:center;z-index:99 !important;color:red;}</style><div> <div id="myProgress" style="width: 100%; background-color: grey;"><div id="myBar" style="width:1%; height:30px; background-color:green;"></div></div><div id ="displayText"><div id ="recordingInstructionsStyle"> <h3><a href="https://testface.projectoblio.com/demos/"><font size="+10" color="red">←</font></a><a id="recordingInstructions" ><font color="red">Hold anywhere to record. Or, <a href="https://testface.projectoblio.com">select a new filter.</a></font></a><a href="https://testface.projectoblio.com/demos/"><font size="+10" color="red">→</font></a></h3></div> <h2 id="pleaseWait">Please wait...</h2></id><div id ="canvasContainer" class="canvasContainer"> <canvas class="mycanvas" width="600" height="600" id="jeeFaceFilterCanvas"></canvas></div> <div id="filter"></div> </div>';
-	//main();
-	setTimeout(function(){
-		var pw = document.getElementById("pleaseWait");
-		pw.style="visibility:hidden";
-	},7000);
 	
-        //wrapper.innerHTML = '<header><i class="material-icons" id="menu-open">menu</i><span class="title">Touch Menu L.A.</span></header><div class="center-icon"><i class="material-icons arrow">keyboard_backspace</i><i class="material-icons">touch_app</i><div class="text">Drag</div></div><div id="menu" class="touch-menu-la"><div class="inner-header">Touch Menu<span>Like Android</span></div><ul class="menu-items"><li><a href="https://github.com/ericktatsui/Touch-Menu-Like-Android"><i class="fa fa-github"></i> Github</a></li><li><a href="mailto:ericktatsui@gmail.com"><i class="fa fa-envelope"></i> ericktatsui@gmail.com</a></li></ul><div class="inner-footer">el risus. Pellentesque facilisis blandit auctor. Maecenas vestibulum vulputate tincidunt. Mauris nec quam libero. Fusce eget ligula non leo varius condimentum quis ac elit.</div><div class="inner-footer"><iframe src="https://ghbtns.com/github-btn.html?user=ericktatsui&repo=Touch-Menu-Like-Android&type=star&count=true" frameborder="0" scrolling="0" width="160px" height="30px"></iframe></div></div>'
-	//wrapper.innerHtml='<div id="myProgress" style="width: 100%; background-color: grey;"><div id="myBar" style="width:1%; height:30px; background-color:green;"></div></div>'
-        
-	var stopped=false;
-	var replaying=false;
-	var moveStarted=false;
-	var move = function() {
-   		 var elem = document.getElementById("myBar"); 
-   		 var width = 1;
-   		 var id = setInterval(frame, 10);
-    		function frame() {
-        		if (width >= 100 || stopped) {
-            			clearInterval(id);
-				elem.style.width="1%";
-        		} else {
-           		 	width++; 
-        		        elem.style.width = width + '%'; 
-        		}
-    		}
+	function headerLoaded(){
+		setTimeout(function(){
+			var pw = document.getElementById("pleaseWait");
+			pw.style="visibility:hidden";
+		},7000);
+	
+	       
 		
-	}
-	function CI(){
-	
-			clearInterval(id);
-			stopped=true;
-	
-
-	}
-	var element;
-
-	var currTime;
-	function holdBegin(event){
-//		console.log("holdbegin",JSON.stringify(event));
-   		event.preventDefault();
-		currTime=Date.now();
-
-	    	//var touch = event.touches[0];
-		if(loaded){
-			var recordingInstructions=document.getElementById("recordingInstructions");
-			recordingInstructions.style="visibility:hidden;"
-			mediaRecorder.start();
-			mediaRecorder2.start();
-			event.preventDefault();
-			console.log(JSON.stringify(event));
-			if(!replaying){
-				stopped=false;// only if x'd after video record
-			}
-			move();
-			moveStarted=true;
-    		//	var touch = event.touches[0];
- 	  		
-		}//element = document.elementFromPoint(touch.pageX,touch.pageY);
-	}
-
-	document.addEventListener('touchstart',holdBegin, false);
-	document.addEventListener('mousedown',holdBegin, false);
-
-	function holdEnd(event){
-		event.preventDefault();
-		console.log("holdend",JSON.stringify(event),Date.now()-currTime,loaded);
-		if (loaded) {
-			mediaRecorder.stop();	
-			mediaRecorder2.stop();
-			if(Date.now()-currTime>3000){
-				console.log("would upload here");
-				CI();
-				replaying=true;
-				function rereplay(){
-					setTimeout(function(){
-						//console.log("stopped",media1Stopped,media2Stopped);
-						if(media1Stopped && media2Stopped){
-							replay();
-						}else rereplay();
-					},100);
+		var stopped=false;
+		var replaying=false;
+		var moveStarted=false;
+		var move = function() {
+	   		 var elem = document.getElementById("myBar"); 
+	   		 var width = 1;
+	   		 var id = setInterval(frame, 10);
+	    		function frame() {
+				if (width >= 100 || stopped) {
+		    			clearInterval(id);
+					elem.style.width="1%";
+				} else {
+		   		 	width++; 
+				        elem.style.width = width + '%'; 
 				}
-				rereplay();
-				// is inside mediaRecorder2 now 
-				
-			}else{
-				currTime=Date.now();
-				console.warn("Video must be at least 3 seconds to upload");
-				CI(); 
-			}
-    		}
-	}
-	document.addEventListener('touchmove',holdEnd,false);
-	document.addEventListener('mouseup',holdEnd,false);
-
-/*
-	 var header=document.createElement('header');
-	    header.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; top 0; ');
-		header.innerHtml="Replying to.. 'What did the fox say?'";
-	 var footer=document.createElement('footer');
-	    footer.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; bottom 0; ');
-
-	var link1 = document.createElement("a");
-	link1.href="https://testface.projectoblio.com:4443/demos"
-	link1.innerHtml="All filters";
-	link1.setAttribute('style','left 0;');
-	var link2 = document.createElement("a");
-	link2.href="https://testface.projectoblio.com:4443/demos"
-	link2.innerHtml="Some other link";
-	link2.setAttribute('style','right 0;');
-	footer.appendChild(link1);
-	footer.appendChild(link2);
-	//document.body.insertBefore(footer,document.body.firstChild);
-	document.body.insertBefore(header,document.body.firstChild);
-*/
-});
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-/*    var githubRibbon=document.createElement('a');
-    githubRibbon.setAttribute('href', 'https://github.com/jeeliz/jeelizFaceFilter');
-    var githubRibbonImage=document.createElement('img');
-    githubRibbonImage.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
-    githubRibbonImage.setAttribute('alt', 'Fork me on GitHub');
-    githubRibbonImage.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
-    githubRibbon.appendChild(githubRibbonImage);
-
-    document.body.appendChild(githubRibbon);
-*/
-//var script = document.createElement('script');
-//script.type = 'text/javascript';
-//script.src = 'https://cdn.webrtc-experiment.com/MediaStreamRecorder.js';
-//document.head.appendChild(script);
-	
-	console.log("dom content loaded");
-	var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(25);
-	mediaRecorder = new MediaRecorder(stream);
-	var chunks = [];
-	mediaRecorder.ondataavailable=function(e){
-		chunks.push(e.data)
-	}
-	
-	mediaRecorder.onstop=function(e){
-		var blob = new Blob(chunks, { 'type' : 'video/mp4' });
-	 	 chunks = [];
- 	 	videoURL = window.URL.createObjectURL(blob);
-		console.log("heres the file url",videoURL);
-		var link = document.createElement("a"); // Or maybe get it from the current document
-		link.href = videoURL;
-		link.download = "aDefaultFileName.mp4";
-		link.innerHTML = "FILE FROM CANVAS";	
-		link.setAttribute('style', 'position: absolute; top: 250; left: 0; border: 0; z-index: 1000');
-		document.body.appendChild(link); 
-		media1Stopped=true;
+	    		}
 		
-	} // end of media record stop
-	
-	function onMediaError(e){
-		console.log('media error',e);
-	}
-	var chunks2;
-	var link2;
-	function onMediaSuccess(stream){
-		console.log("media success");
-		mediaRecorder2 = new MediaRecorder(stream);
-		//mediaRecorder2.start();
-		chunks2 = [];
-		mediaRecorder2.ondataavailable=function(e){
-			chunks2.push(e.data)
 		}
-		mediaRecorder2.onstop=function(e){
-			var blob = new Blob(chunks2, { 'type' : 'video/mp4' });
-	 		 chunks2 = [];
- 	 		audioURL = window.URL.createObjectURL(blob);
-			console.log("heres the file url",audioURL);
-			link2 = document.createElement("a"); // Or maybe get it from the current document
-			link2.href = audioURL;
-			link2.download = "aDefaultFileName.mp4";
-			link2.innerHTML = "FILE FROM WEBCAM";
-   			 link2.setAttribute('style', 'position: absolute; top: 300; left: 300; border: 0; z-index: 1000');
-			document.body.appendChild(link2); 
-			media2Stopped=true;
+		function CI(){
+	
+				clearInterval(id);
+				stopped=true;
+	
+
+		}
+		var element;
+
+		var currTime;
+		function holdBegin(event){
+	//		console.log("holdbegin",JSON.stringify(event));
+	   		event.preventDefault();
+			currTime=Date.now();
+
+		    	//var touch = event.touches[0];
+			if(loaded){
+				var recordingInstructions=document.getElementById("recordingInstructions");
+				recordingInstructions.style="visibility:hidden;"
+				mediaRecorder.start();
+				mediaRecorder2.start();
+				event.preventDefault();
+				console.log(JSON.stringify(event));
+				if(!replaying){
+					stopped=false;// only if x'd after video record
+				}
+				move();
+				moveStarted=true;
+	    		//	var touch = event.touches[0];
+	 	  		
+			}//element = document.elementFromPoint(touch.pageX,touch.pageY);
+		}
+
+		document.addEventListener('touchstart',holdBegin, false);
+		document.addEventListener('mousedown',holdBegin, false);
+
+		function holdEnd(event){
+			event.preventDefault();
+			console.log("holdend",JSON.stringify(event),Date.now()-currTime,loaded);
+			if (loaded) {
+				mediaRecorder.stop();	
+				mediaRecorder2.stop();
+				if(Date.now()-currTime>3000){
+					console.log("would upload here");
+					CI();
+					replaying=true;
+					function rereplay(){
+						setTimeout(function(){
+							//console.log("stopped",media1Stopped,media2Stopped);
+							if(media1Stopped && media2Stopped){
+								replay();
+							}else rereplay();
+						},100);
+					}
+					rereplay();
+					// is inside mediaRecorder2 now 
+				
+				}else{
+					currTime=Date.now();
+					console.warn("Video must be at least 3 seconds to upload");
+					CI(); 
+				}
+	    		}
+		}
+		document.addEventListener('touchmove',holdEnd,false);
+		document.addEventListener('mouseup',holdEnd,false);
+
+	/*
+		 var header=document.createElement('header');
+		    header.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; top 0; ');
+			header.innerHtml="Replying to.. 'What did the fox say?'";
+		 var footer=document.createElement('footer');
+		    footer.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; bottom 0; ');
+
+		var link1 = document.createElement("a");
+		link1.href="https://testface.projectoblio.com:4443/demos"
+		link1.innerHtml="All filters";
+		link1.setAttribute('style','left 0;');
+		var link2 = document.createElement("a");
+		link2.href="https://testface.projectoblio.com:4443/demos"
+		link2.innerHtml="Some other link";
+		link2.setAttribute('style','right 0;');
+		footer.appendChild(link1);
+		footer.appendChild(link2);
+		//document.body.insertBefore(footer,document.body.firstChild);
+		document.body.insertBefore(header,document.body.firstChild);
+	*/
+	});
+
+	document.addEventListener("DOMContentLoaded", function(event) { 
+	/*    var githubRibbon=document.createElement('a');
+	    githubRibbon.setAttribute('href', 'https://github.com/jeeliz/jeelizFaceFilter');
+	    var githubRibbonImage=document.createElement('img');
+	    githubRibbonImage.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
+	    githubRibbonImage.setAttribute('alt', 'Fork me on GitHub');
+	    githubRibbonImage.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
+	    githubRibbon.appendChild(githubRibbonImage);
+
+	    document.body.appendChild(githubRibbon);
+	*/
+	//var script = document.createElement('script');
+	//script.type = 'text/javascript';
+	//script.src = 'https://cdn.webrtc-experiment.com/MediaStreamRecorder.js';
+	//document.head.appendChild(script);
+	
+		console.log("dom content loaded");
+		var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(15);
+		mediaRecorder = new MediaRecorder(stream);
+		var chunks = [];
+		mediaRecorder.ondataavailable=function(e){
+			chunks.push(e.data)
+		}
+	
+		mediaRecorder.onstop=function(e){
+			var blob = new Blob(chunks, { 'type' : 'video/mp4' });
+		 	 chunks = [];
+	 	 	videoURL = window.URL.createObjectURL(blob);
+			console.log("heres the file url",videoURL);
+			var link = document.createElement("a"); // Or maybe get it from the current document
+			link.href = videoURL;
+			link.download = "aDefaultFileName.mp4";
+			link.innerHTML = "FILE FROM CANVAS";	
+			link.setAttribute('style', 'position: absolute; top: 250; left: 0; border: 0; z-index: 1000');
+			document.body.appendChild(link); 
+			media1Stopped=true;
+		
 		} // end of media record stop
 	
-	// Create a second file stream too. for raw data
-	}
-	navigator.mediaDevices.getUserMedia({"video":true,"audio":true}).then(onMediaSuccess).catch(onMediaError);
+		function onMediaError(e){
+			console.log('media error',e);
+		}
+		var chunks2;
+		var link2;
+		function onMediaSuccess(stream){
+			console.log("media success");
+			mediaRecorder2 = new MediaRecorder(stream);
+			//mediaRecorder2.start();
+			chunks2 = [];
+			mediaRecorder2.ondataavailable=function(e){
+				chunks2.push(e.data)
+			}
+			mediaRecorder2.onstop=function(e){
+				var blob = new Blob(chunks2, { 'type' : 'video/mp4' });
+		 		 chunks2 = [];
+	 	 		audioURL = window.URL.createObjectURL(blob);
+				console.log("heres the file url",audioURL);
+				link2 = document.createElement("a"); // Or maybe get it from the current document
+				link2.href = audioURL;
+				link2.download = "aDefaultFileName.mp4";
+				link2.innerHTML = "FILE FROM WEBCAM";
+	   			 link2.setAttribute('style', 'position: absolute; top: 300; left: 300; border: 0; z-index: 1000');
+				document.body.appendChild(link2); 
+				media2Stopped=true;
+			} // end of media record stop
+	
+		// Create a second file stream too. for raw data
+		}
+		navigator.mediaDevices.getUserMedia({"video":true,"audio":true}).then(onMediaSuccess).catch(onMediaError);
 
-	loaded=true;
+		loaded=true;
+	}
+	
 });
