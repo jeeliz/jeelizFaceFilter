b03d59541be3d40d3395000c480be5ee7e6f6243
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-10 22:59:32 -0400

    update

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -33,23 +33,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	link.download = "aDefaultFileName.mp4";
 	link.innerHTML = "Click here to download the file";
     link.setAttribute('style', 'position: absolute; top: 0; left: 0; border: 0; z-index: 1000');
-    link.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
-    link.setAttribute('alt', 'Fork me on GitHub');
-    link.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
+    //link.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
+   // link.setAttribute('alt', 'Fork me on GitHub');
+   // link.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
 		document.body.appendChild(link); 
 
 		alert(link);
-//	}
-	setTimeout(function(){mediaRecorder.stop()},10*1000
-
-
-
-
-
-
-
-
-
+	} // end of media record stop
+	setTimeout(function(){mediaRecorder.stop()},10*1000);
 	// Create a second file stream too. for raw data
 
 
