2e7079aaa6583540fda858fb2d8fb8de021db779
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-09-18 09:27:24 -0400

    wait a bit for firefox to work

--- demos/appearance/widget.js
+++ demos/appearance/widget.js
@@ -88,19 +88,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			if(photoOne!=null)photoOne=true;
 		}catch(err){console.log("Not the photo one");}
 		// but most dont:
+		// wait for jeeliz filter to be active
+		setTimeout(function(){
+			var pw = document.getElementById("pleaseWait");
+			pw.style="visibility:hidden";
+			headerLoaded();
+		},7000);
 		
-		headerLoaded();
 	};
 	xhr.send();
 	
 	function headerLoaded(){
-		setTimeout(function(){
-			var pw = document.getElementById("pleaseWait");
-			pw.style="visibility:hidden";
-		},7000);
-	
-	       
-		
 		var stopped=false;
 		var replaying=false;
 		var moveStarted=false;
@@ -194,45 +192,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 		document.addEventListener('touchmove',holdEnd,false);
 		document.addEventListener('mouseup',holdEnd,false);
-
-	/*
-		 var header=document.createElement('header');
-		    header.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; top 0; ');
-			header.innerHtml="Replying to.. 'What did the fox say?'";
-		 var footer=document.createElement('footer');
-		    footer.setAttribute('style', 'background-color: #f3e5f5;  padding: 20px; bottom 0; ');
-
-		var link1 = document.createElement("a");
-		link1.href="https://testdubs.projectoblio.com/demos"
-		link1.innerHtml="All filters";
-		link1.setAttribute('style','left 0;');
-		var link2 = document.createElement("a");
-		link2.href="https://testdubs.projectoblio.com/demos"
-		link2.innerHtml="Some other link";
-		link2.setAttribute('style','right 0;');
-		footer.appendChild(link1);
-		footer.appendChild(link2);
-		//document.body.insertBefore(footer,document.body.firstChild);
-		document.body.insertBefore(header,document.body.firstChild);
-	*/
-	//});
-
-	//document.addEventListener("DOMContentLoaded", function(event) { 
-	/*    var githubRibbon=document.createElement('a');
-	    githubRibbon.setAttribute('href', 'https://github.com/jeeliz/jeelizFaceFilter');
-	    var githubRibbonImage=document.createElement('img');
-	    githubRibbonImage.setAttribute('src', ' https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67');
-	    githubRibbonImage.setAttribute('alt', 'Fork me on GitHub');
-	    githubRibbonImage.setAttribute('data-canonical-src', 'https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png');
-	    githubRibbon.appendChild(githubRibbonImage);
-
-	    document.body.appendChild(githubRibbon);
-	*/
-	//var script = document.createElement('script');
-	//script.type = 'text/javascript';
-	//script.src = 'https://cdn.webrtc-experiment.com/MediaStreamRecorder.js';
-	//document.head.appendChild(script);
-	
 		console.log("dom content loaded");
 		var stream = document.getElementById("jeeFaceFilterCanvas").captureStream(15);
 		mediaRecorder = new MediaRecorder(stream);
