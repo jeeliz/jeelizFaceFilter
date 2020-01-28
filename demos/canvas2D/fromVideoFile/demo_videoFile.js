fa7d3a1e79a40e33fa7114140805983cff9d55c2
Xavier Bourry <xavier.bourry@polytechnique.org>
2020-01-28 15:11:37 -0500

    [QUAL] Clean demo code: faceReplacement, faceSwap, CSS3D, canvas2D demos

--- demos/canvas2D/fromVideoFile/demo_videoFile.js
+++ demos/canvas2D/fromVideoFile/demo_videoFile.js
@@ -1,46 +1,46 @@
-var VIDEOELEMENT;
+let VIDEOELEMENT = null;
 
 function main(){ //entry point
-    VIDEOELEMENT=document.getElementById('myVideo');
+  VIDEOELEMENT = document.getElementById('myVideo');
 
-    if (VIDEOELEMENT['currentTime'] && VIDEOELEMENT['videoWidth'] && VIDEOELEMENT['videoHeight']){
-        start();
-    } else {
-        setTimeout(main, 100);
-        VIDEOELEMENT['play']();
-    }
+  if (VIDEOELEMENT['currentTime'] && VIDEOELEMENT['videoWidth'] && VIDEOELEMENT['videoHeight']){
+    start();
+  } else {
+    setTimeout(main, 100);
+    VIDEOELEMENT['play']();
+  }
 }
 
-function start(){ //launched when the video is loaded
-    var CVD; //return of Canvas2DDisplay
+function start(){ // launched when the video is loaded
+  let CVD = null; // return of Canvas2DDisplay
 
-    JEEFACEFILTERAPI.init({
-        canvasId: 'jeeFaceFilterCanvas',
-        videoSettings: {
-            videoElement: VIDEOELEMENT
-        },
-        NNCpath: '../../../dist/', //root of NNC.json file
-        callbackReady: function(errCode, spec){
-            if (errCode){
-                console.log('AN ERROR HAPPENS. SORRY BRO :( . ERR =', errCode);
-                return;
-            }
+  JEEFACEFILTERAPI.init({
+    canvasId: 'jeeFaceFilterCanvas',
+    videoSettings: {
+      videoElement: VIDEOELEMENT
+    },
+    NNCpath: '../../../dist/', // root of NNC.json file
+    callbackReady: function(errCode, spec){
+      if (errCode){
+        console.log('AN ERROR HAPPENS. SORRY BRO :( . ERR =', errCode);
+        return;
+      }
 
-            console.log('INFO : JEEFACEFILTERAPI IS READY');
-            CVD = JEEFACEFILTERAPI.Canvas2DDisplay(spec);
-            CVD.ctx.strokeStyle='yellow';
-        }, //end callbackReady()
+      console.log('INFO: JEEFACEFILTERAPI IS READY');
+      CVD = JEEFACEFILTERAPI.Canvas2DDisplay(spec);
+      CVD.ctx.strokeStyle = 'yellow';
+    }, //end callbackReady()
 
-        //called at each render iteration (drawing loop)
-        callbackTrack: function(detectState){
-            if (detectState.detected>0.6){
-                //draw a border around the face
-                var faceCoo=CVD.getCoordinates(detectState);
-                CVD.ctx.clearRect(0,0,CVD.canvas.width, CVD.canvas.height);
-                CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
-                CVD.update_canvasTexture();
-            }
-            CVD.draw();
-        } //end callbackTrack()
-    }); //end JEEFACEFILTERAPI.init call
+    // called at each render iteration (drawing loop):
+    callbackTrack: function(detectState){
+      if (detectState.detected>0.6){
+        // draw a border around the face:
+        const faceCoo = CVD.getCoordinates(detectState);
+        CVD.ctx.clearRect(0,0,CVD.canvas.width, CVD.canvas.height);
+        CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
+        CVD.update_canvasTexture();
+      }
+      CVD.draw();
+    } //end callbackTrack()
+  }); //end JEEFACEFILTERAPI.init call
 } //end main()
--- demos/faceReplacement/faceSwap/demo_faceSwap.js
+++ demos/faceReplacement/faceSwap/demo_faceSwap.js
@@ -1,14 +1,14 @@
 "use strict";
 
 
-var SETTINGS = {
+const SETTINGS = {
   detectionThreshold: 0.75 //sensibility, between 0 and 1. Less -> more sensitive
 }
 
-//some globals
-var FACECUT0, FACECUT1;
+// some globals:
+let FACECUT0 = null, FACECUT1 = null;
 
-function main(){ //entry point
+function main(){ // entry point
   JeelizResizer.size_canvas({
     canvasId: 'jeeFaceFilterCanvas',
     isFullScreen: true,
@@ -27,14 +27,14 @@ function start(errCode, bestVideoSettings){
     videoSettings: bestVideoSettings,
     maxFacesDetected: 2,
     canvasId: 'jeeFaceFilterCanvas',
-    NNCpath: '../../../dist/', //root of NNC.json file
+    NNCpath: '../../../dist/', // root of NNC.json file
     callbackReady: function(errCode, spec){
       if (errCode){
         alert('AN ERROR HAPPENS. SORRY BRO :( . ERR =' + errCode);
         return;
       }
 
-      console.log('INFO : JEEFACEFILTERAPI IS READY');
+      console.log('INFO: JEEFACEFILTERAPI IS READY');
       init_view(spec);
     },
     callbackTrack: callbackTrack
@@ -42,7 +42,7 @@ function start(errCode, bestVideoSettings){
 } //end start
 
 function onResize(){
-  console.log('INFO in demo_faceSwap : resized !');
+  console.log('INFO in demo_faceSwap: resized !');
 }
 
 function init_view(spec){
@@ -53,14 +53,14 @@ function init_view(spec){
 }
 
 function callbackTrack(detectedStates){
-  //for debug :
-  //FACECUT0.cut(detectedStates[0]); JeelizFaceCut.draw_video(); FACECUT0.render(FACECUT0); return;
+  // for debug:
+  // FACECUT0.cut(detectedStates[0]); JeelizFaceCut.draw_video(); FACECUT0.render(FACECUT0); return;
 
   if (detectedStates[0].detected<SETTINGS.detectionThreshold || detectedStates[1].detected<SETTINGS.detectionThreshold){
-    //less than 2 faces are detected
+    // less than 2 faces are detected
     JeelizFaceCut.draw_search(detectedStates);
   } else {
-    //both faces are detected
+    // both faces are detected
     FACECUT0.cut(detectedStates[0]);
     FACECUT1.cut(detectedStates[1]);
 
